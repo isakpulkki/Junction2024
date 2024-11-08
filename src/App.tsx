93bf4e42c34766f305a7bfeb6801cb9bf86384e9
isakpulkki <isak@pulkki.me>
2024-11-08 23:37:33 +0200

    Authentication bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AuthenticationBar from './components/AuthenticationBar'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const theme = createTheme({
+  
+  colorSchemes: {
+    dark: {palette: {
+      primary: {main: '#f5f5f5'},
+    }},
+}, typography: {
+  fontFamily: 'monospace',
+  button: {
+    textTransform: "none",
+    fontFamily: 'monospace',
+    letterSpacing: '.1rem'
+  }
+} 
+});
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <ThemeProvider theme={theme}>
+    <AuthenticationBar></AuthenticationBar>
+      </ThemeProvider>
   )
 }
 
---
+++ src/components/AuthenticationBar.tsx
@@ -0,0 +1,39 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import LockIcon from '@mui/icons-material/Lock';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+
+function AuthenticationBar() {
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters sx={{
+            justifyContent: "space-between"
+          }}>
+          <Typography
+            variant="h5"
+            noWrap
+            sx={{
+              mr: 2,
+              letterSpacing: '.3rem'
+            }}
+          >
+            Voice
+          </Typography>
+          <Box sx={{ flexGrow: 0}}>
+          <Button variant="contained" color="primary" >
+              <LockIcon sx={{mr: 1}}/> Authenticate
+            </Button>
+          </Box>
+          
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default AuthenticationBar;
