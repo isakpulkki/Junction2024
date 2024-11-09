f31757225781e7ea78c7ce4f4c9c580d3cc6b274
isakpulkki <isak@pulkki.me>
2024-11-09 15:45:44 +0200

    Refactor code

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,12 +1,8 @@
-
 import AuthenticationBar from '../components/AuthenticationBar';
 import { ThemeProvider } from '@mui/material/styles';
-import { theme } from '../App'; 
+import { theme } from '../App';
 
 export default function HistoryPage() {
-
-
-
   return (
     <ThemeProvider theme={theme}>
       <AuthenticationBar />
