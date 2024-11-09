221b08f11c4ade60bf461b2f34492e7fa20f8900
isakpulkki <isak@pulkki.me>
2024-11-09 02:41:13 +0200

    Fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import AuthenticationBar from './components/AuthenticationBar';
 import Voting from './components/Voting';
-import Settings from './components/Settings';
-import SingleVoting from './components/SingleVoting';
-import VotingHistory from './components/VotingHistory';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useEffect, useState } from 'react';
 
